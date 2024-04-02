d6d063740b30db5791ce97a936df7d68ac754486
RevaNayla <150822644+RevaNayla@users.noreply.github.com>
2024-04-02 18:40:49 +0700

    Create Linear equation.java

---
+++ Linear equation.java
@@ -0,0 +1,52 @@
+public class LinearEquation {
+    private double a, b, c, d, e, f;
+
+    public LinearEquation() {
+        // Constructor tanpa argumen
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public double getD() {
+        return d;
+    }
+
+    public double getE() {
+        return e;
+    }
+
+    public double getF() {
+        return f;
+    }
+
+    public void setCoefficients(double a, double b, double c, double d, double e, double f) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        this.e = e;
+        this.f = f;
+    }
+
+    public boolean isSolvable() {
+        return (a * d - b * c) != 0;
+    }
+
+    public double getX() {
+        return (e * d - b * f) / (a * d - b * c);
+    }
+
+    public double getY() {
+        return (a * f - e * c) / (a * d - b * c);
+    }
+}
